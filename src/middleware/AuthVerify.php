97c567fcd02c87641ed596f68d1a7b63893e4c71
kain <zhangtqx@vip.qq.com>
2019-10-31 17:48:57 +0800

    Update Auth

--- src/middleware/AuthVerify.php
+++ src/middleware/AuthVerify.php
@@ -4,7 +4,11 @@ declare (strict_types=1);
 
 namespace think\support\middleware;
 
+use think\facade\Cookie;
+use think\redis\library\RefreshToken;
 use think\Request;
+use think\support\facade\Context;
+use think\support\facade\Token;
 
 /**
  * 授权认证验证中间件
@@ -17,7 +21,7 @@ abstract class AuthVerify
      * 场景
      * @var string
      */
-    protected $scene;
+    protected $scene = 'default';
 
     /**
      * @param Request $request
@@ -26,5 +30,52 @@ abstract class AuthVerify
      */
     public function handle(Request $request, \Closure $next)
     {
+        try {
+            if (!Cookie::has($this->scene . '_token')) {
+                return json([
+                    'error' => 1,
+                    'msg' => 'please first authorize user login'
+                ], 401);
+            }
+
+            $tokenString = Cookie::get($this->scene . '_token');
+            $result = Token::verify($this->scene, $tokenString);
+            /**
+             * @var $token \Lcobucci\JWT\Token
+             */
+            $token = $result->token;
+            $symbol = $token->getClaim('symbol');
+            if ($result->expired) {
+                $jti = $token->getClaim('jti');
+                $ack = $token->getClaim('ack');
+                $verify = RefreshToken::create()->verify($jti, $ack);
+                if (!$verify) {
+                    return json([
+                        'error' => 1,
+                        'msg' => 'refresh token verification expired'
+                    ], 401);
+                }
+                $preTokenString = (string)Token::create(
+                    $this->scene,
+                    $jti,
+                    $ack,
+                    (array)$symbol
+                );
+                if (empty($preTokenString)) {
+                    return json([
+                        'error' => 1,
+                        'msg' => 'create token failed'
+                    ]);
+                }
+                Cookie::set($this->scene . '_token', $preTokenString);
+            }
+            Context::set('auth', $symbol);
+            return $next($request);
+        } catch (\Exception $e) {
+            return json([
+                'error' => 1,
+                'msg' => $e->getMessage()
+            ], 400);
+        }
     }
 }
--- src/traits/Auth.php
+++ src/traits/Auth.php
@@ -1,10 +1,136 @@
 <?php
-
 declare (strict_types=1);
 
 namespace think\support\traits;
 
+use think\facade\Cookie;
+use think\helper\Str;
+use think\redis\library\RefreshToken;
+use think\support\facade\Token;
+use think\support\facade\Utils;
+
 trait Auth
 {
+    /**
+     * Set RefreshToken Expires
+     * @return int
+     */
+    protected function __refreshTokenExpires()
+    {
+        return 604800;
+    }
 
+    /**
+     * Create Cookie Auth
+     * @param string $scene
+     * @param array $symbol
+     * @return array
+     */
+    protected function __create(string $scene, array $symbol = [])
+    {
+        $jti = Utils::uuid()->toString();
+        $ack = Str::random();
+        $result = RefreshToken::create()->factory($jti, $ack, $this->__refreshTokenExpires());
+        if (!$result) {
+            return [
+                'error' => 1,
+                'msg' => 'refresh token set failed'
+            ];
+        }
+
+        $tokenString = (string)Token::create($scene, $jti, $ack, $symbol);
+        if (!$tokenString) {
+            return [
+                'error' => 1,
+                'msg' => 'create token failed'
+            ];
+        }
+        Cookie::set($scene . '_token', $tokenString);
+        return [
+            'error' => 0,
+            'msg' => 'ok'
+        ];
+    }
+
+
+    /**
+     * Auth Verify
+     * @param $scene
+     * @return array
+     */
+    protected function __verify($scene)
+    {
+        try {
+            if (!Cookie::has($scene . '_token')) {
+                return [
+                    'error' => 1,
+                    'msg' => 'refresh token not exists'
+                ];
+            }
+
+            $tokenString = Cookie::get($scene . '_token');
+            $result = Token::verify($scene, $tokenString);
+            if ($result->expired) {
+                /**
+                 * @var $token \Lcobucci\JWT\Token
+                 */
+                $token = $result->token;
+                $jti = $token->getClaim('jti');
+                $ack = $token->getClaim('ack');
+                $verify = RefreshToken::create()->verify($jti, $ack);
+                if (!$verify) {
+                    return [
+                        'error' => 1,
+                        'msg' => 'refresh token verification expired'
+                    ];
+                }
+                $symbol = (array)$token->getClaim('symbol');
+                $preTokenString = (string)Token::create(
+                    $scene,
+                    $jti,
+                    $ack,
+                    $symbol
+                );
+                if (!$preTokenString) {
+                    return [
+                        'error' => 1,
+                        'msg' => 'create token failed'
+                    ];
+                }
+                Cookie::set($scene . '_token', $preTokenString);
+            }
+
+            return [
+                'error' => 0,
+                'msg' => 'ok'
+            ];
+        } catch (\Exception $e) {
+            return [
+                'error' => 1,
+                'msg' => $e->getMessage()
+            ];
+        }
+    }
+
+    /**
+     * Destory Auth
+     * @param string $scene
+     * @return array
+     */
+    protected function __destory(string $scene)
+    {
+        if (Cookie::has($scene . '_token')) {
+            $tokenString = Cookie::get($scene . '_token');
+            $token = Token::get($tokenString);
+            RefreshToken::create()->clear(
+                $token->getClaim('jti'),
+                $token->getClaim('ack')
+            );
+            Cookie::delete($scene . '_token');
+        }
+        return [
+            'error' => 0,
+            'msg' => 'ok'
+        ];
+    }
 }
