0df69e3e7e3b6ff6a35b41a02038bb46bd73bdd2
kain <zhangtqx@vip.qq.com>
2020-02-20 16:37:21 +0800

    更新

--- src/facade/Token.php
+++ src/facade/Token.php
@@ -2,15 +2,16 @@
 
 namespace think\support\facade;
 
-use think\extra\contract\TokenInterface;
+use stdClass;
 use think\Facade;
+use think\extra\contract\TokenInterface;
 
 /**
  * Class Token
  * @package think\support\facade
  * @method static \Lcobucci\JWT\Token|false create(string $scene, string $jti, string $ack, array $symbol = [])
  * @method static \Lcobucci\JWT\Token get(string $tokenString)
- * @method static \stdClass verify(string $scene, string $tokenString)
+ * @method static stdClass verify(string $scene, string $tokenString)
  */
 class Token extends Facade
 {
