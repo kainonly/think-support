18eaf9c9f100cca3fb7dadc2cf974569aef2ea73
kain <zhangtqx@vip.qq.com>
2020-12-22 18:43:37 +0800

    更新

--- src/facade/Oss.php
+++ src/facade/Oss.php
@@ -12,6 +12,7 @@ use OSS\OssClient;
  * @package think\support\facade
  * @method static string put(string $name) 上传至对象存储
  * @method static OssClient getClient(bool $extranet = false) 获取对象存储客户端
+ * @method static array generatePostPresigned(array $conditions, int $expired = 600) 生成客户端上传OSS对象存储签名
  */
 class Oss extends Facade
 {
