c12e7ec091a901480c5a8993e1cd0880dd456107
kain <zhangtqx@vip.qq.com>
2020-12-24 17:56:13 +0800

    更新

--- src/facade/Cos.php
+++ src/facade/Cos.php
@@ -12,6 +12,7 @@ use OSS\OssClient;
  * @package think\support\facade
  * @method static string put(string $name) 上传至对象存储
  * @method static array getOption() 获取对象存储配置
+ * @method static array generatePostPresigned(array $conditions, int $expired = 600) 生成客户端上传 COS 对象存储签名
  */
 class Cos extends Facade
 {
