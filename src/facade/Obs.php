af353bf478b0c83bf4e530cc20ff4864ca8d7939
kain <zhangtqx@vip.qq.com>
2020-12-25 09:14:43 +0800

    更新

--- src/facade/Obs.php
+++ src/facade/Obs.php
@@ -12,6 +12,7 @@ use think\Facade;
  * @package think\support\facade
  * @method static string put(string $name) 上传至对象存储
  * @method static ObsClient getClient() 获取对象存储客户端
+ * @method static array generatePostPresigned(array $conditions, int $expired = 600) 生成客户端上传 OBS 对象存储签名
  */
 class Obs extends Facade
 {
